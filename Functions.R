bec0612ec6e755c033b20b2f870df668429e6111
Foxh0und <amiritis@wolfmail.co>
2018-10-16 20:39:27 +1100

    Change variable Name

--- Functions.R
+++ Functions.R
@@ -1,12 +1,12 @@
 createLabels <- function( aTable, show )
 {
-  a <- vector( length = dim( aTable )[1] )
+  lLabels <- vector( length = dim( aTable )[1] )
   for(i in 1:( dim(aTable)[1] ) )
   {
     if( i %% show == 0)
-      a[i] <- rownames( aTable )[i]
+      lLabels[i] <- rownames( aTable )[i]
     else
-      a[i] <- ""
+      lLabels[i] <- ""
   }
-  a
+  lLabels
 }
